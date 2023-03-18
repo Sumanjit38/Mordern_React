c8bb0fafd38d017734a3046291887675077a0de1
Sumanjit38 <sumanjit38@gmail.com>
2023-03-18 21:01:04 +0530

    use state

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    //let name = "Sumanjit";
+    const [name, setName] = useState("Sumanjit");
+    const [age, setAge] = useState(26);
+
+    const handleClick = () => {
+        setName("Chirag");
+        setAge(60);
+        //console.log("Hello Kuity");
+    }
     return (
         <div className="Home">
             <h2>Home Page</h2>
+            {/* <p>{ name }</p> */}
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click Here</button>
         </div>
     );
 }
