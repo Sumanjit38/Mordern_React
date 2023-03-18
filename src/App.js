bae95c5c384863e95d96729c18afca6f2d89478e
Sumanjit38 <sumanjit38@gmail.com>
2023-03-18 20:19:18 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 //import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com";
+  //const title = 'Welcome to the new blog';
+  //const likes = 50;
+  //const link = "http://www.google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className='content'>
-        <h1>{ title }</h1>
-        {/* <h1>App Component</h1> */}
-        <p>Liked { likes } times</p>
+        {/* <h1>{ title }</h1> */}
+        <h1>App Component</h1>
+        <Home />
+        {/* <p>Liked { likes } times</p>
         <p>{ 10 }</p>
         <p>{ "Hello" }</p>
         <p>{ Math.random() * 20 }</p>
-        <a href= {link}>Google Site</a>
+        <a href= {link}>Google Site</a> */}
 
       </div>
     </div>
