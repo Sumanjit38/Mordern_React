b0ddff2131b80a77160104c3e236daf118e86055
Sumanjit38 <sumanjit38@gmail.com>
2023-03-19 02:58:29 +0530

    Fetching data using useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,10 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-    const [name, setName] = useState("mario");
+    const [blogs, setBlogs] = useState(null);
+    //const [name, setName] = useState("mario");
 
+    const [isPending, setIsPending] = useState(true);
     //let name = "Sumanjit";
     //const [name, setName] = useState("Sumanjit");
    // const [age, setAge] = useState(26);
@@ -18,26 +15,41 @@ const Home = () => {
     //     setAge(60);
     //     //console.log("Hello Kuity");
     // }
-    const handleDelete = (id) => {
+    /*const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
-    }
+    }*/
     
     useEffect(() => {
-        console.log("use effect ran");
-        console.log(name);
-    }, [name]);
+        //console.log("use effect ran");
+        //console.log(name);
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+        .then(res => {
+            
+            return res.json()
+        })
+        .then((data => {
+            setBlogs(data);
+            setIsPending(false);
+        }))
+        .catch((err) => {
+            console.log(err.message);
+        })
+        }, 200);
+    }, []);
 
     return (
         <div className="Home">
-            <BlogList blogs = {blogs} title = "All Blogs !" handleDelete = {handleDelete} />
+            { isPending && <div>Loading...</div> }
+            {blogs && <BlogList blogs = {blogs} title = "All Blogs !" />}
             {/* <BlogList blogs = {blogs.filter((blog) => blog.author === "mario")} title = "Mario's blogs"/> */}
             {/* <h2>Home Page</h2> */}
             {/* <p>{ name }</p> */}
             {/* <p>{name} is {age} years old</p> */}
             {/* <button onClick={handleClick}>Click Here</button> */}
-            <button onClick={() => setName("luigi")}>change name </button>
-            <p>{ name }</p>
+            {/* <button onClick={() => setName("luigi")}>change name </button>
+            <p>{ name }</p> */}
         </div>
     );
 }
